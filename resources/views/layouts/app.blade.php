f494954fed049bc1a57b81166e4eb0c8f77f72f8
KeitaIshimatsu <keita.imo.0928@gmail.com>
2026-02-25 10:53:32 +0800

    kei-ver002

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -162,10 +162,14 @@
             </div>
         </main>
     </div>
+<<<<<<< Updated upstream
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
         AOS.init();
     </script>
+=======
+    @yield('scripts')
+>>>>>>> Stashed changes
 </body>
 
 </html>
