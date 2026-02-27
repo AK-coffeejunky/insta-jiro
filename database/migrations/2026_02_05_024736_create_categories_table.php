4eb73d72de84b5bf633ae881bea2c718f246cfee
AK-coffeejunky <ak.coffee.junky@gmail.com>
2026-02-27 11:57:03 +0800

    fixed

--- database/migrations/2026_02_05_024736_create_categories_table.php
+++ database/migrations/2026_02_05_024736_create_categories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categorises', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50)->unique();
             $table->timestamps();
